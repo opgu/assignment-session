016ba4381496f621834e5d8e747a6555c6a80e2e
opgu <99020484+opgu@users.noreply.github.com>
2022-03-03 02:22:22 +0530

    Add files via upload

---
+++ assignment 2.0 all function of strings.py
@@ -0,0 +1,28 @@
+#strings
+
+string = "c++ is great"
+capitalized_string = string.capitalize()
+print('capitalized string: ', capitalized_string)
+
+
+title = 'python programming'
+print(title.encode())
+
+
+message = 'python is fun'
+print(message.endswith('fun'))
+
+string = 'xyz\t34567\tabc'
+print('original string: ', str)
+
+
+
+
+
+
+
+
+
+
+
+
