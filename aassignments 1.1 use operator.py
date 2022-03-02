acbda924a0b0333f224db39adb867e94d80b484d
opgu <99020484+opgu@users.noreply.github.com>
2022-03-03 02:18:22 +0530

    Add files via upload

---
+++ aassignments 1.1 use operator.py
@@ -0,0 +1,75 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:20:19) [MSC v.1925 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Arithmetic operator :
+>>> x = 6
+>>> y = 4
+>>> print(x+y)
+10
+>>> x = 8
+>>> y = 4
+>>> print(x-y)
+4
+>>> x = 6
+>>> y = 7
+>>> print(6*7)
+42
+>>> x = 9
+>>> y = 3
+>>> print(x/y)
+3.0
+>>> x = 7
+>>> y = 5
+>>> print(x%y)
+2
+>>> x = 6
+>>> y = 5
+>>> print(x**y)
+7776
+>>> x = 8
+>>> y = 4
+>>> print(x//Y)
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    print(x//Y)
+NameError: name 'Y' is not defined
+>>> x = 9
+>>> y = 8
+>>> print(x//y)
+1
+>>> 
+====================================================================== RESTART: Shell ======================================================================
+>>> #Assignment operator :
+>>> a = 6
+>>> print(a)
+6
+>>> a += 5
+>>> print(a)
+11
+>>> a -= 9
+>>> print(a)
+2
+>>> a *= 5
+>>> print (a)
+10
+>>> a /= 8
+>>> print(a)
+1.25
+>>> 
+====================================================================== RESTART: Shell ======================================================================
+>>> #Comparision operator :
+>>> a > 10
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    a > 10
+NameError: name 'a' is not defined
+>>> a>10
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    a>10
+NameError: name 'a' is not defined
+>>> #logicaloperator :
+>>> a = 4
+>>> b = 5
+>>> a = 4 and b = 5
+SyntaxError: cannot assign to operator
+>>> 
