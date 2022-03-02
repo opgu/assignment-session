18cc1b0bd50873ca6f74013719f03def31588f73
opgu <99020484+opgu@users.noreply.github.com>
2022-03-03 02:21:09 +0530

    Add files via upload

---
+++ assignment 2.0 list and dictionary.py
@@ -0,0 +1,26 @@
+num =("22","33","44")
+
+print(num)
+
+num =["22","33","44"]
+
+num[0] = '55'
+
+print(num)
+
+#list
+
+mylist = ["html","css","java"]
+
+mylist[1] = 'c++'
+
+print(mylist)
+
+#dictionary
+
+mydict = {
+    "c": "easy",
+    "c++": "moderate",
+    "java": "tough"
+}
+
